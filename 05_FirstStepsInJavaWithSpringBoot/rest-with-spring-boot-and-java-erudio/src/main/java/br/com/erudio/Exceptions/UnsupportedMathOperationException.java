9b5d215248d935be4300902bc348202e539a6810
Elizabeth-Barcena <beth_barcena@hotmail.com>
2023-08-09 14:31:45 -0300

    pathparams e tratamento de exceções

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Exceptions/UnsupportedMathOperationException.java
@@ -0,0 +1,15 @@
+package br.com.erudio.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UnsupportedMathOperationException extends RuntimeException{
+	
+	public UnsupportedMathOperationException(String ex) {
+		super(ex);
+		// TODO Auto-generated constructor stub
+	}
+	private static final long serialVersionUID=1L;
+	
+}
